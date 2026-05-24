07ebdcf80b9eb2004a20d02b03279170a9bcb8d1
Krishna Kodali <krishnakodali81@gmail.com>
2026-05-24 16:49:35 -0400

    feat: init spring backend app

---
+++ backend/src/main/java/com/capitalvantage/backend/CapitalVantageApplication.java
@@ -0,0 +1,11 @@
+package com.capitalvantage.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalVantageApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalVantageApplication.class, args);
+	}
+}
